c4ca948588caff695e486c2d9d5bc49946fab4ae
robert-virtual <robertocastill63@gmail.com>
2022-07-04 09:48:37 -0600

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+let inputs = document.querySelectorAll('input[type="radio"')
+// 0 exelente
+// 1 muy bueno
+// 2 bueno
+// 3 regular
+// 4 malo
+
+// cambia el valor de esta variable segun la calificacion que quieres dar
+let calificacion = 4 // malo
+
+
+inputs.forEach((_e, i) => {
+  if ((i + 1) % 5 == 0) {
+    inputs[i - calificacion].checked = true
+  }
+})
